b23a09716fdf8757f88ba6bbfbf47502a4faf55b
mhd329 <mhd329@gmail.com>
2022-11-28 17:37:49 +0900

    model

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,14 @@ from imagekit.processors import ResizeToFill
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class Art(models.Model):
+    artist = models.ForeignKey(
+        AUTH_USER_MODEL,
+        verbose_name="작가",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    art_category = models.CharField(max_length=50)
+    introduce = models.CharField(max_length=200)
+    price = models.PositiveIntegerField(blank=True, null=True)
