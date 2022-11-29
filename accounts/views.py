86d420703f47e0d8b334883092e817613b688145
choikeunyoung <ought119@gmail.com>
2022-11-29 16:42:14 +0900

    feat:login 페이지 꾸미기
    login.html css 작성 및 화면과 동일하게 구성함.
    Close #22

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,15 @@ from django.contrib.auth import get_user_model
 
 # Create your views here.
 
+
 def index(request):
-  return render(request, "accounts/index.html")
+    return render(request, "accounts/index.html")
+
 
 def signup(request):
 
-  return render(request, "accounts/signup.html")
+    return render(request, "accounts/signup.html")
+
 
 def login(request):
-
-  return render(request, "accounts/login.html")
+    return render(request, "accounts/login.html")
