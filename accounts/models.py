a72412eb28bac5ef2f6435917402311c61ac94b6
Myeonghak Lee <mhmh779@naver.com>
2022-11-29 17:36:22 +0900

    feat:add function #25

--- accounts/models.py
+++ accounts/models.py
@@ -5,52 +5,8 @@ from imagekit.models import ProcessedImageField
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class TimeStampedModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
 class User(AbstractUser):
-    # 선택 항목 필드
-    fullname = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True,
-    )
-    stagename = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True,
-    )
-    email = models.EmailField(
-        null=True,
-        blank=True,
-        max_length=254,
-        verbose_name="이메일 주소",
-    )
-    is_studentID_verified = models.BooleanField(default=False)
-    is_email_verified = models.BooleanField(default=False)
-    is_artist = models.BooleanField(default=False)
-    profile_picture = ProcessedImageField(
-        upload_to="profile_pictures/",
-        null=True,
-        blank=True,
-        processors=[ResizeToFill(512, 512)],
-        format="JPEG",
-        options={
-            "quality": 60,
-        },
-    )
-    career = models.CharField(max_length=200)
-    address = models.CharField(max_length=50)
-    detail_address = models.CharField(max_length=50)
-    followings = models.ManyToManyField(
-        "self", symmetrical=False, related_name="followers"
-    )
+    profileimage = models.ImageField(upload_to="profile/", blank=True)
+    nickname = models.CharField(max_length=40, blank=True)
+    refresh_token = models.TextField(blank=True)
 
-
-    def __str__(self):
-        return self.username
