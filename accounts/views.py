d120e981e3b32214a75f8187709032a6962bb777
Myeonghak Lee <mhmh779@naver.com>
2022-11-29 13:52:34 +0900

    add html

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+def index(request):
+  return render(request, "accounts/index.html")
+
+def signup(request):
+
+  return render(request, "accounts/signup.html")
+
+def login(request):
+
+  return render(request, "accounts/login.html")
