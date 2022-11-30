995519023216520d9de227eb490fba1f94bdd005
choikeunyoung <ought119@gmail.com>
2022-11-30 11:35:42 +0900

    feat: 회원가입 html 과 Model
    회원 가입 html에서 POST 요청시 DB에 값이 저장되게 연동함.
    Close #33

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,10 @@ from django import forms
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        model = get_user_model()
         fields = (
             "username",
             "email",
+            "nickname",
+            "address",
         )
-        model = get_user_model()
