7e593479ec76aae7dd677295aaab9d517b5d8294
ererink <yelkim0210@gmail.com>
2022-11-30 21:36:41 +0900

    feat: 장바구니, 주문 생성 및 삭제, 배송상태 추가
    - 장바구니 추가 (오류 포함)
    - 주문 생성 및 삭제 (오류 포함)
    - 주문 및 작품 별 배송상태
    - articles/detail에서 좋아요, 댓글 기능 추가

--- orders/urls.py
+++ orders/urls.py
@@ -4,8 +4,16 @@ from . import views
 app_name = "orders"
 
 urlpatterns = [
-    path("info/", views.info, name="info"),
+    path("<int:art_pk>/info/", views.info, name="info"),
     path("basket/", views.basket, name="basket"),
     path("payment/", views.payment, name="payment"),
     path("complete/", views.complete, name="complete"),
+    path("create_order/", views.create_order, name="create_order"),
+    path("complete_order/", views.complete_order, name="complete_order"),
+    path("order_delete/", views.order_delete, name="order_delete"),
+    path("order_list/", views.order_list, name="order_list"),
+    path("delivery/", views.delivery, name="delivery"),
+    path("delivery_complete/", views.delivery_complete, name="delivery_complete"),
+    path("mycart/", views.mycart, name="mycart"),
+    path("add_cart/<int:art_pk>", views.add_cart, name="add_cart"),
 ]
