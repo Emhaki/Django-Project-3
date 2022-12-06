59b8f184f8c863feb028706e68dbe724dbd520fb
ererink <yelkim0210@gmail.com>
2022-12-06 16:59:52 +0900

    feat: articles 메인 페이지 검색 기능 구현 #120
    - 작품 제목, 작품 설명, 작가 이름으로 검색
    - 검색 페이지 페이지네이션
    - 검색 후 카테고리 화면으로 다시 돌아가기 링크 연결

--- articles/urls.py
+++ articles/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('<int:pk>/comments/', views.comment_create, name='comment_create'),
     path('<int:pk>/comments/<int:comment_pk>/comment_delete/', views.comment_delete, name='comment_delete'),
     path('<int:pk>/like/', views.like, name='like'),
+    # path("search/", views.SearchView.as_view(), name="search"),
+    path("search/", views.search, name="search"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -5,18 +5,21 @@ from .models import Art, Comment
 from .forms import ArtForm, CommentForm
 from django.core.paginator import Paginator
 import re
+from django.views.generic import ListView
+from django.db.models import Q
 
 def main(request):
+    
     arts = Art.objects.order_by('pk')
     # 작품 카테고리
     category = ["동양화", "서양화", "판화", "일러스트", "조각 및 조소", "설치미술", "사진"]
     art_type_all = "모든 작품"
-    art_type = re.sub(r"[0-9]", "", request.GET.get("type"))
+    # art_type = re.sub(r"[0-9]", "", request.GET.get("type"))
 
     # 페이지네이션
     paginator = Paginator(arts, 8)
     page_number = request.GET.get("type")
-
+    
     if request.GET.get("type"):
         name = re.sub(r"[0-9]", "", request.GET.get("type"))
         arts = Art.objects.filter(art_category__contains=name)
@@ -28,7 +31,6 @@ def main(request):
             'name': name,
             'page_obj': page_obj,
             'category': category, 
-            'art_type': art_type,
             'art_type_all': art_type_all,
         }
         return render(request, "articles/main.html", context)
@@ -126,3 +128,75 @@ def like(request, pk):
         article.likes.add(request.user)
         
     return redirect('articles:detail', pk)
+
+# class SearchView(ListView):
+#     model = Art
+#     context_object_name = "arts_list"
+#     template_name = "articles/search.html"
+#     paginate_by = 8
+    
+#     def get_queryset(self):
+#         search_keyword = self.request.GET.get("q", "")
+#         arts_list = Art.objects.order_by('-id')
+#         return Art.objects.filter(
+#             Q(title__icontains=search_keyword)
+#             | Q(artist__icontains=search_keyword)
+#         )
+        
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         paginator = context["paginator"]
+#         page_numbers_range = 5
+#         max_index = len(paginator.page_range)
+        
+#         page = self.request.GET.get("page")
+#         current_page = int(page) if page else 1
+        
+#         start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+#         end_index = start_index + page_numbers_range
+        
+#         if end_index >= max_index:
+#             end_index = max_index
+        
+#         page_range = paginator.page_range[start_index:end_index]
+#         context["page_range"] = page_range
+        
+#         search_keyword = self.request.GET.get("q", "")
+        
+#         if len(search_keyword) > 1:
+#             context["q"] = search_keyword
+
+#         return context
+
+def search(request):
+    all_data = Art.objects.order_by("-pk")
+    search = request.GET.get("search")
+    paginator = Paginator(all_data, 6)
+    page_obj = paginator.get_page(request.GET.get("page"))
+    category = ["동양화", "서양화", "판화", "일러스트", "조각 및 조소", "설치미술", "사진"]
+
+    if search:
+        search_list = all_data.filter(
+            Q(title__icontains=search)
+            | Q(content__icontains=search) 
+            | Q(artist__nickname__icontains=search) 
+            )
+        paginator = Paginator(search_list, 6)
+        page_obj = paginator.get_page(request.GET.get("page"))
+        context = {
+            "search": search,
+            "search_list": search_list,
+            "question_list": page_obj,
+            "category": category,
+        }
+    else:
+        if search == None :
+            search = ''
+            
+        context = {
+            "search": search,
+            "search_list": all_data,
+            "question_list": page_obj,
+            "category": category,
+        }
+    return render(request, "articles/search.html", context)
