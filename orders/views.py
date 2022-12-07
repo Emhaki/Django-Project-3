7bed97fe908ae8e6ea2a28570b959b55a249e9c9
mhd329 <mhd329@gmail.com>
2022-12-08 02:18:24 +0900

    fix: orders 카트 작품 제거시 금액이 바뀌지 않던 현상 수정
    1. 이제 결제 시 약관에 동의해야만 결제할 수 있음
    2. 카트에서 물건 뺐을 때 금액 안바뀌는것 수정
    3. 카트에 물건이 없을 때 결제창으로 안넘어가게 임시로 수정
    - iframe 안에 스크롤 바 적용은 안 되는 것 같다.
    Close #172

--- orders/views.py
+++ orders/views.py
@@ -104,7 +104,7 @@ def add_cart(request, art_pk):
         )
         cart.save()
         in_cart = True
-
+    
     # context = {
     #     "in_cart": in_cart,
     # }
@@ -118,8 +118,14 @@ def delete_cart(request, art_pk):
     if request.method == "POST":
         target = my_cart.get(art__pk=my_pick.pk)
         target.delete()
+        
+        total_price_after_delete = 0
+        for item in my_cart:
+            total_price_after_delete += item.art.price
+        
         context = {
             "artPk": art_pk,
+            "total_price_after_delete" : total_price_after_delete,
         }
         return JsonResponse(context)
     else:
