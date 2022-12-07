1e550df763241a5107e8f7ff77a22f3d4d6cfc95
Myeonghak Lee <mhmh779@naver.com>
2022-12-08 01:52:21 +0900

    feat: 문의페이지 비동기 댓글 추가 #168

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
+from django.http import  JsonResponse
 
 # Create your views here.
 def index(request):
@@ -21,9 +22,32 @@ def create(request):
 
 def myquestion(request):
   questions = Question.objects.filter(user=request.user.pk)
+  question = Question.objects.get(pk=request.user.pk)
+  comments = question.comment_set.all().order_by('-pk')
 
   context = {
     "questions": questions,
+    "comments": comments,
   }
 
   return render(request, "questions/myquestion.html", context)
+
+def comment_create(request, question_pk):
+    question_comment = get_object_or_404(Question, pk=question_pk)
+    
+    if request.user.is_authenticated:
+        commentForm = CommentForm(request.POST)
+        if commentForm.is_valid():
+            comment = commentForm.save(commit=False)
+            comment.question = question_comment
+            comment.user = request.user
+            comment.save()
+            context = {
+              'content': comment.content,
+              'userName': comment.user.username,
+              'created_at': comment.created_at
+            }
+
+        return JsonResponse(context)
+    else:
+        return redirect("accounts:login")
